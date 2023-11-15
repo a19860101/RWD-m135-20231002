bac19ca1055acf12693417f12de6f84a8c3aff5d
Zac <a19860101@gmail.com>
2023-11-15 09:42:26 +0800

    20231115

---
+++ TAILWIND/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary:'#fcd34d'
+            },
+            spacing: {
+                '25': '100px'
+            }
+        }
+    }
+}
