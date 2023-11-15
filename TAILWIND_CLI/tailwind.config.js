53b33edf90a9dacae9d5412da136fbb90bd8aca9
Zac <a19860101@gmail.com>
2023-11-15 10:57:54 +0800

    20231115

--- TAILWIND_CLI/tailwind.config.js
+++ TAILWIND_CLI/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          primary:'#fcd34d'
+      },
+      spacing: {
+          '25': '100px'
+      }
+    },
+    fontFamily:{
+        'sans':['Exo\\ 2','Noto Sans TC'],
+        'serif':['DM Serif Display','Noto Serif TC']
+    },
+    screens: {
+        'sm':'600px',
+        'md':'768px',
+        'lg':'960px',
+        'xl':'1200px'
+    }
   },
   plugins: [],
 }
